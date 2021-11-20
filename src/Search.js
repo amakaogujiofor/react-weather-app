dea859da6c02ceee70a1a2e4cb7f518f2d91c79d
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-11-20 16:35:21 +0100

    Added Javascript added

---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <div>
+      <div className="search-engine">
+        <input type="text" placeholder="Type a city"></input>
+        <input type="submit"></input>
+      </div>
+    </div>
+  );
+}
