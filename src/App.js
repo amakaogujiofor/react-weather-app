7b359c2fd4af72362dcd885636c2445a328127b4
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-11-21 12:03:33 +0100

    Included created components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Search from "./Search";
+import Location from "./Location";
+import Temperature from "./Temperature";
 
 function App() {
   return (
     <div className="App">
       <Search />
+      <Location />
+      <Temperature />
     </div>
   );
 }
